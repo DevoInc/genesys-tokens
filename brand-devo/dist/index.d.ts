cba8919267843589e13cd2724d205428edf0dee3
Gonzalo Uceda <gonzalo.uceda@devo.com>
2022-09-06 09:06:40 +0200

    style(brand-devo): use single quotes

--- brand-devo/dist/index.d.ts
+++ brand-devo/dist/index.d.ts
@@ -1,5 +1,5 @@
-import { Brand as Light } from "./light/js";
-import { Brand as Dark } from "./dark/js";
+import { Brand as Light } from './light/js';
+import { Brand as Dark } from './dark/js';
 
 export declare const light: Light;
 export declare const dark: Dark;
