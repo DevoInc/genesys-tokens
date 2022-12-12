d78eaf7342801c5f5f82ad1e16ea638048fad4a0
Jose Andrés Granero <joseandres.granero@devo.com>
2022-12-12 14:17:10 +0100

    Adjustments to export the meta info (scheme type: light or dark) for JS output

--- tokens/src/filters/isCmpAlias/matcher.ts
+++ tokens/src/filters/isCmpAlias/matcher.ts
@@ -3,4 +3,4 @@ import type { TransformedToken } from 'style-dictionary';
 import { TOKENS_TIER } from '../../constants';
 
 export const matcher = (token: TransformedToken) =>
-  [TOKENS_TIER.CMP, TOKENS_TIER.ALIAS].includes(token.attributes?.tier);
+  [TOKENS_TIER.CMP, TOKENS_TIER.ALIAS, TOKENS_TIER.META].includes(token.attributes?.tier);
