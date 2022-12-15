66ec14486556f7efc7c0f7a53f482685583b51dd
Jose Andrés Granero <joseandres.granero@devo.com>
2022-12-15 18:44:36 +0100

    Fixed tests

--- tokens/src/filters/isCmpAlias/matcher.ts
+++ tokens/src/filters/isCmpAlias/matcher.ts
@@ -3,4 +3,6 @@ import type { TransformedToken } from 'style-dictionary';
 import { TOKENS_TIER } from '../../constants';
 
 export const matcher = (token: TransformedToken) =>
-  [TOKENS_TIER.CMP, TOKENS_TIER.ALIAS, TOKENS_TIER.META].includes(token.attributes?.tier);
+  [TOKENS_TIER.CMP, TOKENS_TIER.ALIAS, TOKENS_TIER.META].includes(
+    token.attributes?.tier
+  );
