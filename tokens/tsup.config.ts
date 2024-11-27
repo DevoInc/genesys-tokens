06038a2b55599eb2f8a602fb49a6d297dddb5298
Javier López Úbeda <jlopezcur@gmail.com>
2024-11-27 23:42:35 +0100

    build: use default tsconfig

--- tokens/tsup.config.ts
+++ tokens/tsup.config.ts
@@ -4,7 +4,12 @@ export default defineConfig({
   entry: ['src/index.ts'],
   sourcemap: true,
   clean: true,
-  dts: true,
+  dts: {
+    compilerOptions: {
+      moduleResolution: 'bundler',
+    },
+    resolve: true,
+  },
   format: ['esm', 'cjs'],
   shims: true,
   publicDir: true,
